8d6af1876c11a3cb6d979a59da3300abd7f29d43
unelucidated <douglastan27@gmail.com>
2020-10-26 14:19:50 +0800

    tut13

--- index.js
+++ index.js
@@ -27,3 +27,17 @@ tabs.forEach((tab) => {
         })
     })
 })
+
+//modal
+
+const signupButton = document.querySelector('#signup');
+const modalBg = document.querySelector('.modal-background');
+const modal = document.querySelector('.modal');
+
+signupButton.addEventListener('click', () => {
+    modal.classList.add('is-active');
+});
+
+modalBg.addEventListener('click', () => {
+    modal.classList.remove('is-active');
+})
